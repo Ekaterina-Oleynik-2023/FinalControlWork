ac43fb56e687187c8d73f0291993d8b34ca4b1b5
Ekaterina <santeh.lg@gmail.com>
2023-04-25 13:16:00 +0300

    Решение задачи в коде

---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа, которая из имеюшегося массива строк формирует массив,
+// длина строк которых не больше 3-х символов.
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++);
+    Console.WriteLine($"Начальный массив: [{string.Join(" ", array)}] ");
+}
+Console.WriteLine();
+ void FinalArray(string[] newarray){
+     string[] finalarray = new string[newarray.Length];
+     for (int i = 0; i < newarray.Length; i++){
+         if (newarray[i].Length <= 3){
+             finalarray[i] = newarray[i];
+         }
+     }
+     Console.WriteLine($"Конечный  массив: [{string.Join(" ", finalarray)}] ");
+ }
+Console.Clear();
+string[] array = { "лиса", "кот", "ёж", "ворона", "чиж", "жук", "медведь"};
+PrintArray(array);
+FinalArray(array);
